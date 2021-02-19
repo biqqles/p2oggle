06304b985fc081bdfe8d7c20da6ee28b94c428de
biqqles <biqqles@protonmail.com>
2021-02-19 12:09:53 +0000

    Update copyright notices for 2021

--- app/src/main/java/eu/biqqles/p2oggle/Shell.kt
+++ app/src/main/java/eu/biqqles/p2oggle/Shell.kt
@@ -1,5 +1,5 @@
 /*
- *  Copyright © 2019-2020 biqqles.
+ *  Copyright © 2019-2021 biqqles.
  *
  *  This Source Code Form is subject to the terms of the Mozilla Public
  *  License, v. 2.0. If a copy of the MPL was not distributed with this
